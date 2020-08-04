f347d5cbfafc456b508c25119541744cfb7aaeae
Mark Du <mark_du1@baylor.edu>
2020-08-03 20:37:37 -0500

    Initial work

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+from discord.emoji import Emoji
+
+token = open("token.txt", "r").read()  # read token from txt file
+
+#Command prefix is .
+client = commands.Bot(command_prefix='.')
+
+# @bot.command()
+# async def echo(ctx, arg):
+# 	if arg == "baboon1":
+# 		await ctx.send("B1")
+# 	elif arg == "baboon2":
+# 		await ctx.send("B2")
+
+@client.event
+async def on_ready():
+    print('Bot ready')
+
+@client.event
+async def on_member_join(member):  # method expected by client. This runs once when connected
+    print(f'{member} has joined the server')  # notification of login.
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has quit! Boo!')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Test')
+
+#Type .joke for an "in your pants joke"
+@client.command()
+async def joke(ctx):
+    await ctx.send('It\'s in your pants!')
+
+
+client.run('Njg5OTE4NDE5NDUzMzQ1ODE5.XnJ2mQ.JAfp7D7P4UXchVgFxMj1ACQ1KeU')  # token from text file
--- disc_bot_basic.py
+++ disc_bot_basic.py
@@ -1,10 +1,24 @@
 
 import discord
+import os
+import random
+
 # print(discord.__version__)  # check to make sure at least once you're on the right version!
+from discord.ext import commands
 
 token = open("token.txt", "r").read()  # read token from txt file
 
 client = discord.Client()  # starts the discord client.
+
+#Command prefix is !bbn or !Bbn
+bot = commands.Bot(command_prefix=("!bbn", "!Bbn"), case_insensitive=True)
+
+@bot.command()
+async def echo(ctx, arg):
+	if arg == "baboon1":
+		await ctx.send("B1")
+	elif arg == "baboon2":
+		await ctx.send("B2")
 
 
 @client.event  # event decorator/wrapper. More on decorators here: https://pythonprogramming.net/decorators-intermediate-python-tutorial/
@@ -13,19 +27,28 @@ async def on_ready():  # method expected by client. This runs once when connecte
 
 
 @client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, ARE YOU READY TO FEED YOUR ASS OFF?'
+    )
+    await member.channel.send(
+        f'Hi {member.name}, ARE YOU READY TO FEED YOUR ASS OFF?'
+    )
+
+@client.event
 async def on_message(message):  # event that happens per any message.
 
-    # each message has a bunch of attributes. Here are a few.
-    # check out more by print(dir(message)) for example.
-    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+	# This prevents the bot from recursively printing its own message
+	if message.author == client.user:
+		return
 
-    if len(message) != 0:
-    	await message.channel.send("ok")
+	funny_quotes = ['ARE YOU READY TO FEED YOUR ASS OFF?', 'hi neighbor', 'got2gitgud kid', 'hi']
 
-# @client.event
-# async def on_message(message):  # event that happens per any message.
-#     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-#     if str(message.author) == "Sentdex#7777" and "hello" in message.content.lower():
-#         await message.channel.send('Hi!')
+	if message.content != ' ':
+		response = random.choice(funny_quotes)
+		await message.channel.send(response)
+	elif message.content == '!hi':
+		await message.channel.send('hi' + {message.name})
 
 client.run(token)  # token from text file
