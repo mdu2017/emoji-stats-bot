921ffea8a484dcbcd4c13d2fdbe924feb75ea189
Mark Du <mark_du1@baylor.edu>
2020-08-19 19:33:49 -0500

    Updating help

--- cogs/general.py
+++ cogs/general.py
@@ -69,82 +69,18 @@ class General(commands.Cog):
                      inline=True)
         em.add_field(name='fullchstats', value='Shows the most popular emoji and react for each channel if possible',
                      inline=True)
+        em.add_field(name='favorite <user>', value='Shows a user\'s favorite emoji '
+                                                   '| <user> -- user nickname or mention', inline=True)
 
         # Add descriptions for General commands
         em.add_field(name='ping', value='Display network latency', inline=True)
 
         await author.send(embed=em)
 
-    # Disable bot commands in specified channel
-    # @commands.command(brief='Disable bot command usage in a specific channel')
-    # async def rmbotch(self, ctx, chan):
-    #     found = False
-    #     for channel in ctx.guild.text_channels:
-    #         if channel.name == chan:
-    #             rm_channels.append(chan)
-    #             print(f'Bot commands disabled for #{chan}')
-    #             found = True
-    #             break
-    #
-    #     if not found:
-    #         await ctx.send(f'Text Channel #{chan} was not found')
-
-    # Enable bot commands in specified channel
-    # @commands.command(brief='Enable bot command usage in a specific channel')
-    # async def addbotch(self, ctx, chan):
-    #     found = False
-    #     for channel in ctx.guild.text_channels:
-    #         if channel.name == chan:
-    #             found = True
-    #             break
-    #
-    #
-    #     if chan in rm_channels:
-    #         rm_channels.remove(chan)
-    #         print(f'Bot commands re-enabled for #{chan}')
-    #     else:
-    #         if not found:
-    #             await ctx.send(f'Text Channel #{chan} was not found')
-    #         else:
-    #             await ctx.send(f'{chan} is already enabled!')
-
-    # Make bot parrot a message
-    # @commands.command(brief='Parrot message back with bot')
-    # async def parrot(self, ctx, *args):
-    #     msg = ' '.join(args)
-    #
-    #     await ctx.channel.purge(limit=1)
-    #     await ctx.send(f'{msg}')
-
-    # @commands.command(brief='Put bot to Idle status')
-    # async def sleep(self, ctx):
-    #     await self.client.change_presence(
-    #         activity=discord.Activity(type=discord.ActivityType.listening, name='Sleeping'),
-    #         status=discord.Status.idle, afk=True)
-    #     await ctx.channel.purge(limit=1)
-
     # Displays network ping
     @commands.command(brief='Displays network latency')
     async def ping(self, ctx):
         await ctx.send(f'{round(self.client.latency * 1000)}ms')
-
-    # Clears an X amount of messages from the channe
-    # @commands.command(brief='Remove <X> amt of messages', aliases=['cls'], pass_context=True)
-    # async def clear(self, ctx, amount=3):
-    #
-    #     # Disable feature for serious/news channel
-    #     if ctx.channel.name == 'serious' or ctx.channel.name == 'baboons-of-pig-york':
-    #         return
-    #
-    #     if amount < 1 or amount > 25:
-    #         await ctx.send('Error: amount needs to be between 1-25')
-    #         return
-    #
-    #     # remove clear command
-    #     await ctx.channel.purge(limit=1)
-    #
-    #     # remove <amount> of messages
-    #     await ctx.channel.purge(limit=amount)
 
 
 def setup(client):
